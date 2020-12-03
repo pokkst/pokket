43cc06729caee1d695e2a85213a418d398cecca7
pokkst <pokkst@protonmail.com>
2020-12-02 22:09:40 -0600

    Remove redundant bouncycastle setup, since dep changed

--- app/src/main/java/xyz/pokkst/pokket/SplashActivity.kt
+++ app/src/main/java/xyz/pokkst/pokket/SplashActivity.kt
@@ -8,7 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.bitcoinj.core.Transaction
+import org.bitcoinj.core.TransactionOutPoint
+import org.bitcoinj.core.bip47.BIP47PaymentCode
+import org.bitcoinj.params.MainNetParams
 import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.util.encoders.Hex
 import xyz.pokkst.pokket.wallet.WalletManager
 import java.io.File
 import java.security.Provider
@@ -18,7 +23,6 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupBouncyCastle()
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -48,22 +52,5 @@ class SplashActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-    }
-
-    /*
-    Read the comment within the method. This is needed so the proper Bouncycastle is loaded, so we can use the Elliptic Curve Diffie-Hellman algorithm for BIP47 common secret calculation.
-     */
-    private fun setupBouncyCastle() {
-        val provider: Provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
-            ?: return
-        if (provider.javaClass == BouncyCastleProvider::class.java) { // BC with same package name, shouldn't happen in real life.
-            return
-        }
-        // Android registers its own BC provider. As it might be outdated and might not include
-        // all needed ciphers, we substitute it with a known BC bundled in the app.
-        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
-        // of that it's possible to have another BC implementation loaded in VM.
-        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
-        Security.insertProviderAt(BouncyCastleProvider(), 1)
     }
 }
