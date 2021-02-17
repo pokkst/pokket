ba1d357ca76a5f80162184678ff64dc8ddf9856d
pokkst <pokkst@protonmail.com>
2021-02-16 23:18:30 -0600

    Display pledge data correctly

--- app/src/main/java/xyz/pokkst/pokket/cash/ui/PledgeEntryView.kt
+++ app/src/main/java/xyz/pokkst/pokket/cash/ui/PledgeEntryView.kt
@@ -8,16 +8,24 @@ import android.widget.TextView
 import org.bitcoinj.core.TransactionOutput
 import xyz.pokkst.pokket.cash.R
 import xyz.pokkst.pokket.cash.util.DateFormatter
+import xyz.pokkst.pokket.cash.util.PriceHelper
 
 class PledgeEntryView {
     companion object {
         fun instanceOf(activity: Activity?, position: Int, plegdesFormatted: ArrayList<Map<String, TransactionOutput>>): View {
             val view = LayoutInflater.from(activity)
                 .inflate(R.layout.pledge_item_cell, null)
-            val text1 =
-                view.findViewById<TextView>(R.id.text1)
+            val txHashText = view.findViewById<TextView>(R.id.text1)
+            val bchAmountText = view.findViewById<TextView>(R.id.text2)
+            val fiatAmountText = view.findViewById<TextView>(R.id.text3)
+
             val utxo = plegdesFormatted[position]["utxo"]
-            text1.text = utxo?.parentTransactionHash.toString()
+            val value = utxo?.value?.toPlainString()
+            val fiatValue = value?.toDouble()?.times(PriceHelper.price)
+            txHashText.text = utxo?.parentTransactionHash.toString()
+            bchAmountText.text = "$value BCH"
+            fiatAmountText.text = "($$fiatValue)"
+
             return view
         }
     }
