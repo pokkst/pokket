ec6da936e03e129758d61922cc20c7a12bf877c3
pokkst <pokkst@protonmail.com>
2021-12-19 15:37:44 -0600

    remove unused resources and add derivation path parser

---
+++ app/src/main/java/xyz/pokkst/pokket/cash/util/DerivationParser.kt
@@ -0,0 +1,38 @@
+package xyz.pokkst.pokket.cash.util
+
+import org.bitcoinj.crypto.ChildNumber
+import org.bitcoinj.crypto.HDPath
+import org.bitcoinj.wallet.KeyChainGroupStructure
+
+class DerivationParser {
+    companion object {
+        fun parse(derivationPath: String): KeyChainGroupStructure? {
+            if(derivationPath.startsWith("m/")) {
+                val mRemoved = derivationPath.replace("m/", "")
+                val split = mRemoved.split("/")
+                val nonPrimeExists = split.any { !it.contains("'") }
+                if(nonPrimeExists) {
+                    return null
+                } else {
+                    return try {
+                        var hdPath: HDPath? = null
+                        split.forEachIndexed { index, s ->
+                            val childNumber = s.replace("'", "").toInt()
+                            hdPath = if (index == 0) {
+                                HDPath.M(ChildNumber(childNumber, true))
+                            } else {
+                                hdPath?.extend(ChildNumber(childNumber, true))
+                            }
+                        }
+                        println("PATH:: $hdPath")
+                        KeyChainGroupStructure { hdPath }
+                    } catch (e: Exception) {
+                        null
+                    }
+                }
+            } else {
+                return null
+            }
+        }
+    }
+}
