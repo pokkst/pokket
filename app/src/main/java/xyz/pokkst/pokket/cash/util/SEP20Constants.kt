d20673d154472d1c8d57fe95f29b4b990a1666a0
pokkst <pokkst@protonmail.com>
2021-12-20 10:47:35 -0600

    prep more sep20 stuff

--- app/src/main/java/xyz/pokkst/pokket/cash/util/SEP20Constants.kt
+++ app/src/main/java/xyz/pokkst/pokket/cash/util/SEP20Constants.kt
@@ -3,17 +3,26 @@ package xyz.pokkst.pokket.cash.util
 import org.web3j.contracts.eip20.generated.ERC20
 import org.web3j.tx.gas.DefaultGasProvider
 import xyz.pokkst.pokket.cash.interactors.WalletInteractor
+import java.math.BigInteger
+
+data class SEP20(val contractAddress: String, val symbol: String, val name: String)
+data class SEP20Balance(val sep20: SEP20, val balance: BigInteger, val lastUpdatedTimeMs: Long)
 
 class SEP20Constants {
     companion object {
         private val walletInteractor = WalletInteractor.getInstance()
-        const val FLEXUSD_ADDRESS = "0x7b2B3C5308ab5b2a1d9a94d20D35CCDf61e05b72"
-        const val EBEN_ADDRESS = "0x77CB87b57F54667978Eb1B199b28a0db8C8E1c0B"
-        const val MIST_ADDRESS = "0x5fA664f69c2A4A3ec94FaC3cBf7049BD9CA73129"
-        const val SPICE_ADDRESS = "0xe11829a7d5d8806bb36e118461a1012588fafd89"
+        val FLEXUSD = SEP20("0x7b2B3C5308ab5b2a1d9a94d20D35CCDf61e05b72", "FLEXUSD", "FlexUSD")
+        val EBEN = SEP20("0x77CB87b57F54667978Eb1B199b28a0db8C8E1c0B", "EBEN", "Green Ben")
+        val MIST = SEP20("0x5fA664f69c2A4A3ec94FaC3cBf7049BD9CA73129", "MIST", "MIST")
+        val SPICE = SEP20("0xe11829a7d5d8806bb36e118461a1012588fafd89", "SPICE", "SPICE")
+        val supportedTokens = listOf(FLEXUSD, EBEN, MIST, SPICE)
 
-        fun getToken(contractAddress: String): ERC20 {
-            return ERC20.load(contractAddress, walletInteractor.getSmartWallet(), walletInteractor.getCredentials(), DefaultGasProvider())
+        fun getToken(sep20: SEP20): ERC20 {
+            return ERC20.load(sep20.contractAddress, walletInteractor.getSmartWallet(), walletInteractor.getCredentials(), DefaultGasProvider())
+        }
+
+        fun getTokenBalance(erc20: ERC20): BigInteger {
+            return erc20.balanceOf(walletInteractor.getSmartAddress()).send()
         }
     }
 }
