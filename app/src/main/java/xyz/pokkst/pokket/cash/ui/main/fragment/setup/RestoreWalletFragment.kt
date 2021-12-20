40f813fdd322bc69c2c95a6f7f54859297fbcd46
pokkst <pokkst@protonmail.com>
2021-12-20 13:50:44 -0600

    fix restore issue with deriv path

--- app/src/main/java/xyz/pokkst/pokket/cash/ui/main/fragment/setup/RestoreWalletFragment.kt
+++ app/src/main/java/xyz/pokkst/pokket/cash/ui/main/fragment/setup/RestoreWalletFragment.kt
@@ -38,26 +38,29 @@ class RestoreWalletFragment : Fragment() {
                     val bip39passphrase = root.bip39_passphrase_edit_text.text.toString().trim()
                     val customPath = root.custom_path_edittext.text.toString().trim()
                     val path = DerivationParser.parse(customPath)
-                    if(path != null) {
-                        StatusBarHelper.setStatusBarColor(
-                            requireActivity(),
-                            R.color.extra_light_grey
-                        )
-                        val intent = Intent(requireActivity(), MainActivity::class.java)
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-                        intent.putExtra(Constants.EXTRA_SEED, seedStr)
-                        intent.putExtra(Constants.EXTRA_NEW, false)
-                        if (bip39passphrase.isNotEmpty()) {
-                            intent.putExtra(Constants.EXTRA_PASSPHRASE, bip39passphrase)
-                        }
-                        if (customPath.isNotEmpty() && root.custom_path_checkbox.isChecked) {
-                            PrefsHelper.instance(context)?.edit()?.putString(Constants.PREF_DERIVATION_PATH, customPath)?.apply()
-                            intent.putExtra(Constants.EXTRA_DERIVATION, customPath)
-                        }
-                        startActivity(intent)
-                    } else {
-                        context?.let { it1 -> Toaster.showToastMessage(it1, "invalid derivation path") }
+                    StatusBarHelper.setStatusBarColor(
+                        requireActivity(),
+                        R.color.extra_light_grey
+                    )
+                    val intent = Intent(requireActivity(), MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.putExtra(Constants.EXTRA_SEED, seedStr)
+                    intent.putExtra(Constants.EXTRA_NEW, false)
+                    if (bip39passphrase.isNotEmpty()) {
+                        intent.putExtra(Constants.EXTRA_PASSPHRASE, bip39passphrase)
                     }
+
+                    if (path != null && customPath.isNotEmpty() && root.custom_path_checkbox.isChecked) {
+                        PrefsHelper.instance(context)?.edit()?.putString(Constants.PREF_DERIVATION_PATH, customPath)?.apply()
+                        intent.putExtra(Constants.EXTRA_DERIVATION, customPath)
+                    }
+
+                    if(path == null && customPath.isNotEmpty() && root.custom_path_checkbox.isChecked) {
+                        context?.let { it1 -> Toaster.showToastMessage(it1, "invalid derivation path") }
+                        return@setOnClickListener
+                    }
+
+                    startActivity(intent)
                 }
             }
         }
